8440820fdeb3be5fe460e3d7534d31e511f35a9c
SoaPatrick <patrick.leisinger@gmail.com>
2024-06-30 10:27:53 +0200

    [UPDATE] improve deployment

--- update_server.sh
+++ update_server.sh
@@ -1,12 +1,11 @@
 ssh -t -t $SP_REMOTE_USER@$SP_REMOTE_HOST << EOF
     cd www/fitness-tracker.soapatrick.com
     php artisan down
-    composer update
+    composer install --no-dev --optimize-autoloader --prefer-dist --no-progress --no-ansi
     php artisan migrate --force
     php artisan icons:clear
     php artisan view:clear
     php artisan icons:cache
-    php artisan storage:link
     php artisan up
     exit
 EOF
