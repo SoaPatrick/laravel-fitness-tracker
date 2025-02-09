b202c55e1f7713f5029c724d06da3063970aaf12
SoaPatrick <patrick.leisinger@gmail.com>
2025-02-09 08:44:17 +0100

    [UPDATE] update all packages

--- update_server.sh
+++ update_server.sh
@@ -2,10 +2,14 @@ ssh -t -t $SP_REMOTE_USER@$SP_REMOTE_HOST << EOF
     cd www/fitness-tracker.soapatrick.com
     php artisan down
     composer install --no-dev --optimize-autoloader --prefer-dist --no-progress --no-ansi
-    php artisan migrate --force
     php artisan icons:clear
     php artisan view:clear
+    php artisan route:clear
+    php artisan cache:clear
+    php artisan filament:optimize
+    php artisan migrate --force
     php artisan icons:cache
+    php artisan filament:cache-components
     php artisan up
     exit
 EOF
